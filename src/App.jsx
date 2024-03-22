ae42df55f147452b517b7ab830a0a19a734bbdbc
Kevin <kevin.lucassg@gmail.com>
2024-03-21 23:11:43 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Navbar from "./Componentes/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
