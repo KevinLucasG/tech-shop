543d52a7236f8295bdc184347f3c45aec7625017
Kevin <kevin.lucassg@gmail.com>
2024-04-08 23:27:48 -0300

    Layout of products

---
+++ src/Componentes/Products/Products.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Products.css";
+
+const Products = () => {
+  return (
+    <div className="ProductList">
+      <div className="product-item">Item 01</div>
+      <div className="product-item">Item 02</div>
+      <div className="product-item">Item 03</div>
+      <div className="product-item">Item 04</div>
+      <div className="product-item">Item 05</div>
+      <div className="product-item">Item 06</div>
+    </div>
+  );
+};
+
+export default Products;
