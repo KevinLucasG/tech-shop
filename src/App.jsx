13f7fbb9fcb3317502bacf3da6b3d6075ef0f353
Kevin <kevin.lucassg@gmail.com>
2024-04-08 23:02:15 -0300

    Cart changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Componentes/Navbar";
-import Products from "./Componentes/Products";
+import Products from "./Componentes/Products/Products.jsx";
 import Home from "./Componentes/Home/Home";
 import About from "./Componentes/About/About"
+import Cart from "./Componentes/Cart/Cart.jsx"
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="about" element={<About/>} />
+          <Route path="cart" element={<Cart/>}/>
         </Routes>
       </div>
     </BrowserRouter>
