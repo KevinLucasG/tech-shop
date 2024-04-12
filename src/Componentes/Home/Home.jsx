ff019c728c266e50c0b028e0413a7d48fd4546ee
Kevin <kevin.lucassg@gmail.com>
2024-04-11 23:50:23 -0300

    Imported products and code changes

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import './Home.css';
+import Products from "../Products/Products";
 const Home = () => {
   return(
   <div className="container">
-    <h2 className="title-offer">Itens em oferta</h2>
+    <h2 className="title-offer">ITENS EM OFERTA</h2>
     
-    <div className="ProductList">
-      <div className="product-item">Item 01</div>
-      <div className="product-item">Item 02</div>
-      <div className="product-item">Item 03</div>
-      <div className="product-item">Item 04</div>
-      <div className="product-item">Item 05</div>
-      <div className="product-item">Item 06</div>
-    </div>
+    <Products/>
   </div>
   
 )};
