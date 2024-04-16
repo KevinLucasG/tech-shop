5eb9bab3eb9ab5f76dd5a062f3a377a22cf97c0e
Kevin <kevin.lucassg@gmail.com>
2024-04-16 01:48:43 -0300

    ProductsData added

---
+++ src/Componentes/Products/productsData.jsx
@@ -0,0 +1,50 @@
+// productsData.js
+
+// Dados dos produtos
+const productsData = {
+    'samsung-galaxy-a14': {
+        id: 'samsung-galaxy-a14',
+        name: 'Samsung Galaxy A14',
+        price: 899,
+        description: 'Detalhes do Samsung Galaxy A14.',
+        imageUrl: '../../assets/galaxy-a14.png'
+    },
+    'iphone-15': {
+        id: 'iphone-15',
+        name: 'Iphone 15',
+        price: 4999,
+        description: 'Detalhes do Iphone 15.',
+        imageUrl: '../../assets/iphone15.png'
+    },
+    'iphone-15-pro': {
+        id: 'iphone-15-pro',
+        name: 'Iphone 15 Pro',
+        price: 6420,
+        description: 'Detalhes do Iphone 15 Pro.',
+        imageUrl: '/path/to/iphone15pro.png'
+    },
+    'samsung-galaxy-s23': {
+        id: 'samsung-galaxy-s23',
+        name: 'Samsung Galaxy S23',
+        price: 2799,
+        description: 'Detalhes do Samsung Galaxy S23.',
+        imageUrl: '/path/to/s23.png'
+    },
+    'redmi-note-13': {
+        id: 'redmi-note-13',
+        name: 'Redmi Note 13',
+        price: 1199,
+        description: 'Detalhes do Redmi Note 13.',
+        imageUrl: '/path/to/note13.png'
+    },
+    'poco-x6-pro': {
+        id: 'poco-x6-pro',
+        name: 'POCO X6 Pro',
+        price: 2140,
+        description: 'Detalhes do POCO X6 Pro.',
+        imageUrl: '/path/to/pocox6.png'
+    }
+};
+
+// Exportando `productsData` como default export
+export default productsData;
