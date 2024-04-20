7a269edfd7ab4c9223044c62246301c3b50ac54e
Kevin <kevin.lucassg@gmail.com>
2024-04-19 22:32:42 -0300

    Fixed the url for the product images to appear

--- src/Componentes/Products/productsData.jsx
+++ src/Componentes/Products/productsData.jsx
@@ -7,42 +7,42 @@ const productsData = {
         name: 'Samsung Galaxy A14',
         price: 899,
         description: 'Detalhes do Samsung Galaxy A14.',
-        imageUrl: '../../assets/galaxy-a14.png'
+        imageUrl: '/src/assets/galaxy-a14.png'
     },
     'iphone-15': {
         id: 'iphone-15',
         name: 'Iphone 15',
         price: 4999,
         description: 'Detalhes do Iphone 15.',
-        imageUrl: '../../assets/iphone15.png'
+        imageUrl: '/src/assets/iphone15.png'
     },
     'iphone-15-pro': {
         id: 'iphone-15-pro',
         name: 'Iphone 15 Pro',
         price: 6420,
         description: 'Detalhes do Iphone 15 Pro.',
-        imageUrl: '/path/to/iphone15pro.png'
+        imageUrl: '/src/assets/iphone15pro.png'
     },
     'samsung-galaxy-s23': {
         id: 'samsung-galaxy-s23',
         name: 'Samsung Galaxy S23',
         price: 2799,
         description: 'Detalhes do Samsung Galaxy S23.',
-        imageUrl: '/path/to/s23.png'
+        imageUrl: '/src/assets/s23.png'
     },
     'redmi-note-13': {
         id: 'redmi-note-13',
         name: 'Redmi Note 13',
         price: 1199,
         description: 'Detalhes do Redmi Note 13.',
-        imageUrl: '/path/to/note13.png'
+        imageUrl: '/src/assets/note13.png'
     },
     'poco-x6-pro': {
         id: 'poco-x6-pro',
         name: 'POCO X6 Pro',
         price: 2140,
         description: 'Detalhes do POCO X6 Pro.',
-        imageUrl: '/path/to/pocox6.png'
+        imageUrl: '/src/assets/pocox6.png'
     }
 };
 
