135bebbe0f22c58aa9785a04d5fa1dde79a88f44
Kevin <kevin.lucassg@gmail.com>
2024-04-20 21:20:19 -0300

    Transformed the prices into decimal numbers

--- src/Componentes/Products/productsData.jsx
+++ src/Componentes/Products/productsData.jsx
@@ -13,7 +13,7 @@ const productsData = {
   "iphone-15": {
     id: "iphone-15",
     name: "Iphone 15",
-    price: 4999,
+    price: 4.999,
     description:
       "O iPhone 15 é o mais recente lançamento da Apple, oferecendo um design elegante, desempenho poderoso e uma câmera de alta qualidade. Equipado com o mais recente chip da Apple, promete uma experiência de usuário suave e eficiente, além de recursos aprimorados de fotografia e vídeo.",
     imageUrl: "/src/assets/iphone15.png",
@@ -21,7 +21,7 @@ const productsData = {
   "iphone-15-pro": {
     id: "iphone-15-pro",
     name: "Iphone 15 Pro",
-    price: 6420,
+    price: 6.420,
     description:
       "O iPhone 15 Pro é a versão premium do novo iPhone 15, oferecendo recursos avançados para usuários exigentes. Ele apresenta uma tela ProMotion com taxa de atualização variável, câmeras aprimoradas com capacidades de fotografia computacional avançada e uma bateria de longa duração. Ideal para quem busca o melhor desempenho e recursos em um smartphone.",
     imageUrl: "/src/assets/iphone15pro.png",
@@ -29,7 +29,7 @@ const productsData = {
   "samsung-galaxy-s23": {
     id: "samsung-galaxy-s23",
     name: "Samsung Galaxy S23",
-    price: 2799,
+    price: 2.799,
     description:
       "O Samsung Galaxy S23 é o carro-chefe da Samsung, oferecendo o que há de mais avançado em termos de desempenho, câmeras e recursos. Com uma tela AMOLED de alta resolução, conjunto de câmeras versátil com recursos avançados de inteligência artificial e um design premium, o Galaxy S23 é uma escolha para quem busca o melhor em smartphones Android.",
     imageUrl: "/src/assets/s23.png",
@@ -37,7 +37,7 @@ const productsData = {
   "redmi-note-13": {
     id: "redmi-note-13",
     name: "Redmi Note 13",
-    price: 1199,
+    price: 1.199,
     description:
       "O Redmi Note 13 é um smartphone intermediário da Xiaomi, conhecido por oferecer um excelente custo-benefício. Com uma tela grande e nítida, uma bateria de longa duração e um desempenho sólido, é uma ótima opção para usuários que desejam um dispositivo confiável para uso diário, sem gastar muito.",
     imageUrl: "/src/assets/note13.png",
@@ -45,7 +45,7 @@ const productsData = {
   "poco-x6-pro": {
     id: "poco-x6-pro",
     name: "POCO X6 Pro",
-    price: 2140,
+    price: 2.140,
     description:
       "O Poco X6 Pro é um smartphone da marca Poco, conhecida por oferecer alto desempenho a preços acessíveis. Equipado com um processador potente, uma tela de alta taxa de atualização, uma configuração de câmera versátil e uma bateria de longa duração, é uma escolha sólida para quem busca um dispositivo com ótimo desempenho e recursos por um preço acessível.",
     imageUrl: "/src/assets/pocox6.png",
