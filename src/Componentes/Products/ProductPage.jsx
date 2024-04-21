d6a94761df4ecb0922b717e1579cab358f60c887
Kevin <kevin.lucassg@gmail.com>
2024-04-20 22:59:32 -0300

    Organized the product purchase layout in a more intuitive way

--- src/Componentes/Products/ProductPage.jsx
+++ src/Componentes/Products/ProductPage.jsx
@@ -1,37 +1,41 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import "./ProductPage.css";
-import productsData from './productsData';
+import productsData from "./productsData";
 
 const ProductPage = () => {
-    const { productId } = useParams(); // Obtenha o productId da URL
-    const product = productsData[productId]; // Busque os dados do produto com base no productId
-    console.log(product.imageUrl)
-    // Verifique se o produto foi encontrado
-    if (!product) {
-        return <p>Produto não encontrado.</p>;
-    }
+  const { productId } = useParams(); // Obtenha o productId da URL
+  const product = productsData[productId]; // Busque os dados do produto com base no productId
+  // Verifique se o produto foi encontrado
+  if (!product) {
+    return <p>Produto não encontrado.</p>;
+  }
 
-    return (
-        <div className="section-product">
-            <div className="product-window">
-                <img
-                    className="img-product-window"
-                    src={product.imageUrl} // Use `product.imageUrl` para a imagem do produto
-                    alt={product.name} // Use `product.name` para o alt text
-                    width="450"
-                    height="340"
-                />
-                <div className="description">
-                    <h2 className="title-product">{product.name}</h2> {/* Use `product.name` para o título */}
-                    <p className="text-description">{product.description}</p> {/* Use `product.description` para a descrição */}
-                    <button className="btn-buy" type="button">
-                        Adicionar ao Carrinho
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="section-product">
+      <div className="product-window">
+        <img
+          className="img-product-window"
+          src={product.imageUrl} // Use `product.imageUrl` para a imagem do produto
+          alt={product.name} // Use `product.name` para o alt text
+          width="450"
+          height="340"
+        />
+        <div className="description">
+          <h2 className="title-product">{product.name}</h2>{" "}
+          {/* Use `product.name` para o título */}
+          <p className="text-description">{product.description}</p>{" "}
+          {/* Use `product.description` para a descrição */}
+        
         </div>
-    );
+        <div className="buy-section" >
+            <p className="price-product">R$ {Number.isInteger(product.price) ? product.price : product.price.toFixed(3)}</p>
+            <button className="btn-buy" type="button">Adicionar ao Carrinho</button>
+        </div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
