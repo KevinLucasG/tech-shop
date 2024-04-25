965e07dc0d6c141659fe47aae706d33fda8c4e4a
Kevin <kevin.lucassg@gmail.com>
2024-04-25 19:59:01 -0300

    Atualização de importações de imagens diretas no arquivo productsData.js

--- src/Componentes/Products/productsData.jsx
+++ src/Componentes/Products/productsData.jsx
@@ -1,6 +1,11 @@
-// productsData.js
 
-// Dados dos produtos
+import a14 from "../../assets/galaxy-a14.png";
+import iphone15 from "../../assets/iphone15.png";
+import iphone15pro from "../../assets/iphone15pro.png";
+import s23 from "../../assets/s23.png";
+import note13 from "../../assets/note13.png";
+import pocox6 from "../../assets/pocox6.png";
+
 const productsData = {
   "samsung-galaxy-a14": {
     id: "samsung-galaxy-a14",
@@ -8,7 +13,7 @@ const productsData = {
     price: 899,
     description:
       " O Samsung Galaxy A14 é um smartphone de nível básico da Samsung, oferecendo um desempenho decente a um preço acessível. Ele possui uma tela LCD de tamanho médio, uma configuração de câmera versátil e uma bateria de longa duração, tornando-o uma escolha sólida para usuários que buscam um dispositivo confiável para tarefas do dia a dia..",
-    imageUrl: "/src/assets/galaxy-a14.png",
+    imageUrl: a14,
   },
   "iphone-15": {
     id: "iphone-15",
@@ -16,15 +21,15 @@ const productsData = {
     price: 4.999,
     description:
       "O iPhone 15 é o mais recente lançamento da Apple, oferecendo um design elegante, desempenho poderoso e uma câmera de alta qualidade. Equipado com o mais recente chip da Apple, promete uma experiência de usuário suave e eficiente, além de recursos aprimorados de fotografia e vídeo.",
-    imageUrl: "/src/assets/iphone15.png",
+    imageUrl: iphone15,
   },
   "iphone-15-pro": {
     id: "iphone-15-pro",
     name: "Iphone 15 Pro",
-    price: 6.420,
+    price: 6.42,
     description:
       "O iPhone 15 Pro é a versão premium do novo iPhone 15, oferecendo recursos avançados para usuários exigentes. Ele apresenta uma tela ProMotion com taxa de atualização variável, câmeras aprimoradas com capacidades de fotografia computacional avançada e uma bateria de longa duração. Ideal para quem busca o melhor desempenho e recursos em um smartphone.",
-    imageUrl: "/src/assets/iphone15pro.png",
+    imageUrl: iphone15pro,
   },
   "samsung-galaxy-s23": {
     id: "samsung-galaxy-s23",
@@ -32,7 +37,7 @@ const productsData = {
     price: 2.799,
     description:
       "O Samsung Galaxy S23 é o carro-chefe da Samsung, oferecendo o que há de mais avançado em termos de desempenho, câmeras e recursos. Com uma tela AMOLED de alta resolução, conjunto de câmeras versátil com recursos avançados de inteligência artificial e um design premium, o Galaxy S23 é uma escolha para quem busca o melhor em smartphones Android.",
-    imageUrl: "/src/assets/s23.png",
+    imageUrl: s23,
   },
   "redmi-note-13": {
     id: "redmi-note-13",
@@ -40,17 +45,17 @@ const productsData = {
     price: 1.199,
     description:
       "O Redmi Note 13 é um smartphone intermediário da Xiaomi, conhecido por oferecer um excelente custo-benefício. Com uma tela grande e nítida, uma bateria de longa duração e um desempenho sólido, é uma ótima opção para usuários que desejam um dispositivo confiável para uso diário, sem gastar muito.",
-    imageUrl: "/src/assets/note13.png",
+    imageUrl: note13,
   },
   "poco-x6-pro": {
     id: "poco-x6-pro",
     name: "POCO X6 Pro",
-    price: 2.140,
+    price: 2.14,
     description:
       "O Poco X6 Pro é um smartphone da marca Poco, conhecida por oferecer alto desempenho a preços acessíveis. Equipado com um processador potente, uma tela de alta taxa de atualização, uma configuração de câmera versátil e uma bateria de longa duração, é uma escolha sólida para quem busca um dispositivo com ótimo desempenho e recursos por um preço acessível.",
-    imageUrl: "/src/assets/pocox6.png",
+    imageUrl: pocox6,
   },
 };
 
-// Exportando `productsData` como default export
+
 export default productsData;
