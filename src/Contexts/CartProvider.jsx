5b887ddd7cbd8146fd158ce6f7553e3d6e64919b
Kevin <kevin.lucassg@gmail.com>
2024-05-14 17:13:16 -0300

    Added cart features

--- src/Contexts/CartProvider.jsx
+++ src/Contexts/CartProvider.jsx
@@ -6,16 +6,36 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
-    console.log(product)
-  };
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id);
 
+      if (existingProduct) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
   const removeFromCart = (productId) => {
-    setCart(cart.filter(item => item.id !== productId));
+    setCart((prevCart) => {
+      const productToRemove = prevCart.find((item) => item.id === productId);
+
+      if (productToRemove.quantity > 1) {
+        return prevCart.map((item) =>
+          item.id === productId ? { ...item, quantity: item.quantity - 1 } : item
+        );
+      } else {
+        return prevCart.filter((item) => item.id !== productId);
+      }
+    });
   };
 
   const clearCart = () => {
-    setCart([]);
+    if (window.confirm("Tem certeza?")) {
+      setCart([]);
+    }
   };
 
   return (
