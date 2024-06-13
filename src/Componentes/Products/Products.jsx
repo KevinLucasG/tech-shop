a9417cadb6270b30a6b225ad779363075b04af41
Kevin <kevin.lucassg@gmail.com>
2024-06-12 23:27:40 -0300

    Console.log removed

--- src/Componentes/Products/Products.jsx
+++ src/Componentes/Products/Products.jsx
@@ -9,7 +9,6 @@ import pocox6 from "../../assets/pocox6.png";
 import { Link } from "react-router-dom";
 
 const Products = () => {
-    console.log(a14);
     return (
         <div className="ProductList">
             
