736d617684e555363a8a061c884ae1420196f35c
Kevin <kevin.lucassg@gmail.com>
2024-06-24 23:32:15 -0300

    Improved display of smaller windows

--- src/Componentes/Products/Products.jsx
+++ src/Componentes/Products/Products.jsx
@@ -15,7 +15,7 @@ const Products = () => {
             <Link to="/product/samsung-galaxy-a14" className="product-item">
                 <img className="img-product" src={a14} alt="Samsung Galaxy A14" width="150" height="200" />
                 <div className="belowbar">
-                    <h3>Samsung Galaxy A14</h3>
+                    <h3 className="product-title">Samsung Galaxy A14</h3>
                     <p className="price">R$ 899</p>
                 </div>
             </Link>
@@ -23,7 +23,7 @@ const Products = () => {
             <Link to="/product/iphone-15" className="product-item">
                 <img className="img-product" src={iphone15} alt="Iphone 15" width="210" height="200" />
                 <div className="belowbar">
-                    <h3>Iphone 15</h3>
+                    <h3 className="product-title">Iphone 15</h3>
                     <p className="price">R$ 4.999</p>
                 </div>
             </Link>
@@ -31,7 +31,7 @@ const Products = () => {
             <Link to="/product/iphone-15-pro" className="product-item">
                 <img className="img-product" src={iphone15pro} alt="Iphone 15 Pro" width="210" height="200" />
                 <div className="belowbar">
-                    <h3>Iphone 15 Pro</h3>
+                    <h3 className="product-title">Iphone 15 Pro</h3>
                     <p className="price">R$ 6.420</p>
                 </div>
             </Link>
@@ -39,7 +39,7 @@ const Products = () => {
             <Link to="/product/samsung-galaxy-s23" className="product-item">
                 <img className="img-product" src={s23} alt="Samsung Galaxy S23" width="200" height="200" />
                 <div className="belowbar">
-                    <h3>Samsung Galaxy S23</h3>
+                    <h3 className="product-title">Samsung Galaxy S23</h3>
                     <p className="price">R$ 2.799</p>
                 </div>
             </Link>
@@ -47,7 +47,7 @@ const Products = () => {
             <Link to="/product/redmi-note-13" className="product-item">
                 <img className="img-product" src={note13} alt="Redmi Note 13" width="180" height="200" />
                 <div className="belowbar">
-                    <h3>Redmi Note 13</h3>
+                    <h3 className="product-title">Redmi Note 13</h3>
                     <p className="price">R$ 1.199</p>
                 </div>
             </Link>
@@ -55,7 +55,7 @@ const Products = () => {
             <Link to="/product/poco-x6-pro" className="product-item">
                 <img className="img-product" src={pocox6} alt="POCO X6 Pro" width="180" height="200" />
                 <div className="belowbar">
-                    <h3>POCO X6 Pro</h3>
+                    <h3 className="product-title">POCO X6 Pro</h3>
                     <p className="price">R$ 2.140</p>
                 </div>
             </Link>
